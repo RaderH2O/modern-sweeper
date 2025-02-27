24bb6723ee4130c19130af930d7d68594c2e8b0b
RaderH2O <raderh2ogt@gmail.com>
2025-02-27 19:50:54 +0330

    UI change

--- ui.py
+++ ui.py
@@ -11,7 +11,7 @@ class UI:
         self.width, self.height = self.surf.get_size()
 
     def draw(self, mines):
-        title = self.font.render("M I N E S W E E P E R", True, (0, 0, 0))
+        title = self.font.render("M I N E S W E E P E R  -  By Radin", True, (0, 0, 0))
         m_text = f"{mines} M I N E S" if mines > 1 else f"{mines} M I N E"
         mines_text = self.font.render(m_text, True, (0, 0, 0))
 
